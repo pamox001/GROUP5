08ee8b8a3bbebbaacc6b778b41d10dda9f6caaa6
pamox001 <113963568+pamox001@users.noreply.github.com>
2023-08-14 15:23:10 -0700

    Update MoneyExchange.java

--- MoneyExchange.java
+++ MoneyExchange.java
@@ -6,12 +6,13 @@ public class MoneyExchange{
   public static void main(String[] args) throws FileNotFoundException{
   Scanner console= new Scanner(new File("USD.txt"));
 	int x=0;
-  do{
+//  do{
 	 Scanner scanner=new Scanner(System.in);
 	 Scanner opt=new Scanner(System.in);
 	 
-    System.out.println(" Hi! What currency would you like to exchange today, you can choose USD, EUR, GBP or JPY ?");
-    String choice=scanner.nextLine();
+	System.out.println(" WELCOME!");
+	System.out.println("What currency would you like to exchange today, you can choose USD, EUR, GBP or JPY ?");
+    String choice=scanner.nextLine().toUpperCase();
     System.out.println(" Do you want to exchange it 1-USD, 2-EUR, 3-GBP, 4-JPY or 5-All?");  
 	int currency=opt.nextInt();  
 	System.out.println(" How much would you like to exchange today? ");    
@@ -28,7 +29,7 @@ public class MoneyExchange{
     }else{
       System.out.println(" Please input a valid choice!");
     }
-    
+    /* UNTIL WE FIURE OUT THE REST
     System.out.println("Do you want exchange money again? Y or N");
       String go =opt.nextLine(); 
         if(go.equals("Y")){
@@ -36,10 +37,32 @@ public class MoneyExchange{
         }else{
           x=0;
         }
-    }while(x==1);
+    }while(x==1); */
   }
    public static void exUSD(Scanner console, int currency, double amount){
- 
+	double p;
+	double n;
+	double d;
+	while (console.hasNext()) {
+		String coin = console.next();
+		double count= console.nextDouble();
+		if(coin.equals("EUR")){
+			p=(amount*count);	//0.91	
+		}else if (coin.equals("GBP")) {
+			n = (amount*count); //0.78
+		} else if (coin.equals("JPN")) {
+			d =(amount*count); //142.48
+		}
+		if(currency==2){
+		System.out.println("USD to EUR: "+ p);
+		}else if(currency==3){
+		System.out.println("USD to GBP: "+ n);
+		}else if(currency==4){
+		System.out.println("USD to JPY: "+ d);
+		} else {
+		System.out.println("USD to EUR: "+ p+"  GBD: "+ n+"  JPY: "+d);
+		}
+	}
    }
 
     public static void exEUR(Scanner console, int currency, double amount){
