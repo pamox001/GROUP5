a189eac68f5aa3198a2ffcc7a1fd06540c47530c
pamox001 <113963568+pamox001@users.noreply.github.com>
2023-08-15 09:59:46 -0700

    Update MoneyExchange.java

--- MoneyExchange.java
+++ MoneyExchange.java
@@ -6,43 +6,51 @@ public class MoneyExchange{
   public static void main(String[] args) throws FileNotFoundException{
   Scanner console= new Scanner(new File("USD.txt"));
 	int x=0;
+	int repeat=0;
 //  do{
 	 Scanner scanner=new Scanner(System.in);
 	 Scanner opt=new Scanner(System.in);
 	 
 	System.out.println(" WELCOME!");
+	
+	do{
 	System.out.println("What currency would you like to exchange today, you can choose USD, EUR, GBP or JPY ?");
     String choice=scanner.nextLine().toUpperCase();
-    System.out.println(" Do you want to exchange it 1-USD, 2-EUR, 3-GBP, 4-JPY or 5-All?");  
-	int currency=opt.nextInt();  
-	System.out.println(" How much would you like to exchange today? ");    
-    double amount= opt.nextDouble();
-   // amount= new Scanner(System.in);
-    if(choice.equals("USD")){
-      exUSD(console,  currency, amount);
-    }else if(choice.equals("EUR")){
-      exEUR(console,  currency, amount);
-    }else if(choice.equals("JPY")){
-      exJPY(console,  currency, amount);
-     }else if(choice.equals("GBP")){
-      exGBP(console,  currency, amount);
-    }else{
-      System.out.println(" Please input a valid choice!");
-    }
-    /* UNTIL WE FIURE OUT THE REST
-    System.out.println("Do you want exchange money again? Y or N");
-      String go =opt.nextLine(); 
-        if(go.equals("Y")){
-          x=1;
-        }else{
-          x=0;
-        }
-    }while(x==1); */
+    //fix so that all string to capetal
+		if(choice!="USD"||choice!="EUR"||choice!="GBP"||choice!="JPY"){
+		      System.out.println(" Please input a valid choice!");
+		      repeat=1;
+		}else{
+			System.out.println(" Do you want to exchange it 1-USD, 2-EUR, 3-GBP, 4-JPY or 5-All?");  
+			int currency=opt.nextInt();  
+			System.out.println(" How much would you like to exchange today? ");    
+			double amount= opt.nextDouble();
+		   // amount= new Scanner(System.in);
+		   
+		   
+			if(choice.equals("USD")){
+			  exUSD(console,  currency, amount);
+			}else if(choice.equals("EUR")){
+			  exEUR(console,  currency, amount);
+			}else if(choice.equals("JPY")){
+			  exJPY(console,  currency, amount);
+			 }else if(choice.equals("GBP")){
+			  exGBP(console,  currency, amount);
+			}
+			
+			System.out.println("Do you want exchange money again? Y or N");
+			  String go=opt.nextLine(); 
+			if(go.equals("Y")){
+					repeat=1;
+			}
+		}
+    }while(repeat==1);
   }
    public static void exUSD(Scanner console, int currency, double amount){
-	double p;
-	double n;
-	double d;
+  	double p=0.00;
+	double n=0.00;
+	double d=0.00;
+	
 	while (console.hasNext()) {
 		String coin = console.next();
 		double count= console.nextDouble();
@@ -59,17 +67,17 @@ public class MoneyExchange{
 		System.out.println("USD to GBP: "+ n);
 		}else if(currency==4){
 		System.out.println("USD to JPY: "+ d);
-		} else {
+			} else if(currency==5){
 		System.out.println("USD to EUR: "+ p+"  GBP: "+ n+"  JPY: "+d);
 		}
 	}
    }
 
     public static void exEUR(Scanner console, int currency, double amount){
-   	double p;
-	double n;
-	double d;
-	double x;
+  	double p=0.00;
+	double n=0.00;
+	double d=0.00;
+	double x=0.00;
 	while (console.hasNext()) {
 		String coin = console.next();
 		double count= console.nextDouble();
@@ -88,16 +96,16 @@ public class MoneyExchange{
 		System.out.println("EUR to GBP: "+ n);
 		}else if(currency==4){
 		System.out.println("EUR to JPY: "+ d);
-		} else {
+			} else if(currency==5){
 		System.out.println("EUR to USD: "+ p+"  GBP: "+ n+"  JPY: "+d);
 		}
 	}
     }
     public static void exGBP(Scanner console, int currency, double amount){
-    	double p;
-	double n;
-	double d;
-	double x;
+  	double p=0.00;
+	double n=0.00;
+	double d=0.00;
+	double x=0.00;
 	while (console.hasNext()) {
 		String coin = console.next();
 		double count= console.nextDouble();
@@ -116,16 +124,17 @@ public class MoneyExchange{
 		System.out.println("GBP to EUR: "+ n);
 		}else if(currency==4){
 		System.out.println("GBP to JPY: "+ d);
-		} else {
+	} else if(currency==5){
 		System.out.println("GBP to USD: "+ p+"  EUR: "+ n+"  JPY: "+d);
 		}
 	}  
     }
 
     public static void exJPY(Scanner console, int currency, double amount){
-  	double p;
-	double n;
-	double d;
+  	double p=0.00;
+	double n=0.00;
+	double d=0.00;
+	double x=0.00;
 	while (console.hasNext()) {
 		String coin = console.next();
 		double count= console.nextDouble();
@@ -144,11 +153,10 @@ public class MoneyExchange{
 		System.out.println("JPY to EUR: "+ n);
 		}else if(currency==3){
 		System.out.println("JPY to GBP: "+ d);
-		} else {
+		} else if(currency==5){
 		System.out.println("JPY to USD: "+ p+"  EUR: "+ n+"  GBP: "+d);
 		}
 	}    
     }
-}
-  
+}  
     
